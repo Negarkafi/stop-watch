b392ef7cb52a3e959243b33b9b2840fa0aad4c18
negar kafi <negark9776@gmail.com>
2024-12-27 23:01:45 +0330

    chore: add vite-tsconfig-paths for improved path resolution and update tsconfig settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import type { InlineConfig } from 'vitest/node';
 import type { UserConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 type ViteConfig = UserConfig & { test: InlineConfig };
 
 const config: ViteConfig = {
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
